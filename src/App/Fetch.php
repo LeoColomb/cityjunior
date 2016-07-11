ddcec35d8f0e51e83e93f16a29b0c3a768e698fb
Léo Colombaro <git@colombaro.fr>
2016-07-11 16:05:14 +0200

    Fixing condition

--- src/App/Fetch.php
+++ src/App/Fetch.php
@@ -57,7 +57,7 @@ class Fetch
         if ($response->getStatusCode() !== 200) {
             throw new \Exception('Unable to login with anwser: '.$response->getReasonPhrase());
         }
-        if ((int) $response->getHeader('Content-Length') < 600) {
+        if ($response->getBody()->getSize() < 1000) {
             throw new \Exception('Connection refused, wrong ids');
         }
     }
